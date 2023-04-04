e77e8f8e9f8a12d51b4e71cb3df3e1175520bac8
FlyingTonight <hikmasterr@gmail.com>
2023-04-04 14:58:49 +0500

    edited

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,12 +11,11 @@
                     <h6 class="text-secondary font-weight-semi-bold text-uppercase mb-3">Latest Blog</h6>
                     <h1 class="section-title mb-3">Oxirgi Postlar</h1>
                 </div>
-
             </div>
             <div class="row">
 
+                    @foreach ($posts as $post )
 
-                @foreach ($posts as $post)
 
 
                 <div class="col-lg-4 col-md-6 mb-5">
@@ -27,25 +26,23 @@
                             <small class="text-white text-uppercase">Jan</small>
                         </div>
                     </div>
-                         <div class="d-flex mb-2">
-                            {{-- @foreach ($post->tags as $tag ) --}}
-
-
-                        {{-- <a class="text-secondary text-uppercase font-weight-medium">{{$tag->name}}</a>
-                        <span class="text-primary px-2">|</span>
-                        @endforeach --}}
-                    </div>
                     <div class="d-flex mb-2">
-                        <a class="text-danger text-uppercase font-weight-medium" href="">{{$post->category->name}}</a>
+                        <a class="text-secondary text-uppercase font-weight-medium" href="">Admin</a>
+                        <span class="text-primary px-2">|</span>
+
+                        <a class="text-secondary text-uppercase font-weight-medium" href="">Cleaning</a>
                     </div>
                     <h5 class="font-weight-medium mb-2">{{$post->title}}</h5>
                     <p class="mb-4">{{$post->short_content}}</p>
                     <a class="btn btn-sm btn-primary py-2" href="{{ route('posts.show',['post'=> $post->id])}}">O'qish</a>
                 </div>
-              @endforeach
-              {{$posts->links()}}
+                @endforeach
 
-                </div>
+
+
+
+
+
             </div>
         </div>
     </div>
